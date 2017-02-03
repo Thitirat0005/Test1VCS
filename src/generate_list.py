b5ffda0b4debb2fd02c6163fe39f360763ee1473
raTar <ch.thitirat_st@tni.ac.th>
2017-02-03 03:13:34 +0000

    Commit 4 - generate_list v2

--- src/generate_list.py
+++ src/generate_list.py
@@ -1,6 +1,8 @@
 import random
 def generate_list():
-    alist = [x for x in range(random.randint(-10,10))]
+    alist=[]
+    for x in range(random.randint(0,20)):
+        alist.append(x)
     return alist
     
 """    
@@ -8,16 +10,22 @@ print a generate list
 """
 
 def printIT():
-    print(generate_list())
+    alist=[]
+    alist=generate_list()
+    print(alist)
+    assert len(alist), "alist is null, please random again"
+    assert sum(alist)>=100, "sum of alist < 100"
+    
     
 def main():
+    []
     printIT()
     
 """
 IF this script file is called, it will run main() directly
 """
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print("Test printIT():" )
     main()
     
